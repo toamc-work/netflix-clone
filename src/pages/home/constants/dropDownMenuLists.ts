7bfdb44d8155439cbe0425a0564e3c19a7cc3599
toamc-work <toamc.work@gmail.com>
2023-11-30 15:42:36 +0200

    Add NavbarSideDropDownMenu constant

---
+++ src/pages/home/constants/dropDownMenuLists.ts
@@ -0,0 +1,32 @@
+export const NavbarSideDropDownMenu = [
+    {
+        name: 'Toam',
+    },
+    {
+        name: 'Hagit',
+    },
+    {
+        name: 'Goni',
+    },
+    {
+        name: 'Meitar',
+    },
+    {
+        name: 'Kids',
+    },
+    {
+        name: 'Manager Porifle',
+    },
+    {
+        name: 'Transfer Profile',
+    },
+    {
+        name: 'Account',
+    },
+    {
+        name: 'Help Center',
+    },
+    {
+        name: 'Sign out of Netflix',
+    },
+] 

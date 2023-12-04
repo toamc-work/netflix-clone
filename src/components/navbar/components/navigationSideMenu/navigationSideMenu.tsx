941b69b4a35eaff40339bb01d8c18c2bf6586524
toamc-work <toamc.work@gmail.com>
2023-12-04 16:10:40 +0200

    Refactor NavigationSideMenu component
    Removed unused imports and state, and simplified hover effect handling. Integrated DropDownMenu component for improved menu structure.

--- src/components/navbar/components/navigationSideMenu/navigationSideMenu.tsx
+++ src/components/navbar/components/navigationSideMenu/navigationSideMenu.tsx
@@ -1,7 +1,9 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 import { FlexBox, NavigationText, PadBox } from '../../../mui/MuiStyledComponents';
-import { ArrowDropDown, Notifications, Search } from '@mui/icons-material';
+import { Notifications, Search } from '@mui/icons-material';
 import { useMediaQuery } from 'usehooks-ts';
+import DropDownMenu from './dropDownMenu/dropDownMenu';
+
 
 interface INavigationSideMenuProps {
     isDesktop: boolean,
@@ -13,38 +15,6 @@ const NavigationSideMenu: React.FC<INavigationSideMenuProps> = ({ isDesktop, ite
     const kidsIconMaxView = useMediaQuery('(max-width: 900px)')
     const NotificationIconMaxView = useMediaQuery('(max-width: 800px)')
 
-    const [isHoverOnMenuIcon, setIsHoverOnMenuIcon] = useState(false);
-    const [isHoverOnMenu, setIsHoverOnMenu] = useState(false);
-
-
-    const handleIsHoverOnMenuIcon = (mouse: 'enter' | 'leave') => {
-        if (mouse === 'enter') {
-            setIsHoverOnMenuIcon(true)
-        }
-
-        if (mouse === 'leave') {
-
-            const timeoutID = setTimeout(() => {
-                setIsHoverOnMenuIcon(() => false);
-                clearTimeout(timeoutID)
-            }, 200);
-        }
-
-    }
-
-    const handleIsHoverOnMenu = (mouse: 'enter' | 'leave') => {
-        if (mouse === 'enter') {
-            setIsHoverOnMenu(true)
-        }
-
-        if (mouse === 'leave') {
-            const timeoutID = setTimeout(() => {
-                setIsHoverOnMenu(() => false);
-                clearTimeout(timeoutID)
-            }, 200);
-        }
-
-    }
 
     return (
         <FlexBox
@@ -93,72 +63,32 @@ const NavigationSideMenu: React.FC<INavigationSideMenuProps> = ({ isDesktop, ite
                         <Notifications sx={{ color: 'white' }} fontSize={'large'} />
                     </PadBox>
                 )}
-                <FlexBox
-                    justifyContent={'center'}
-                    alignItems={'center'}
-                    sx={{
-                        position: 'relative',
-                    }}
-                >
-                    <PadBox
-                        showOutlines={true}
-                        padding={2}
-                        sx={{
-                            backgroundColor: 'white',
-                            height: '3rem',
-                            width: '3rem',
-                            display: 'flex',
-                            alignItems: 'center',
-                            borderRadius: '5px',
-                        }}
-                        onMouseEnter={() => handleIsHoverOnMenuIcon('enter')}
-                        onMouseLeave={() => handleIsHoverOnMenuIcon('leave')}
-
-                    />
-
-                    <ArrowDropDown sx={{
-                        color: 'white',
-                        transition: '0.2s linear',
-                        width: '1.5rem',
-                        height: '1.5rem',
-                        transform: `rotate(${isHoverOnMenuIcon || isHoverOnMenu ? '180deg' : '0deg'})`,
-                    }} fontSize={'large'} />
-                    <FlexBox
-                        position={'absolute'}
-                        right={0}
-                        top={60}
-                        width={'15rem'}
-                        height={'auto'}
-                        showOutlines={true}
-                        flexDirection={'column'}
-                        justifyContent={'center'}
-                        alignContent={'baseline'}
-                        onMouseEnter={() => handleIsHoverOnMenu('enter')}
-                        onMouseLeave={() => handleIsHoverOnMenu('leave')}
-                    >
-                        {(isHoverOnMenuIcon || isHoverOnMenu) && items.map((item, index) => {
-                            return (
-                                <PadBox
-                                    // showOutlines={true}
-                                    key={item.name}
-                                    padding={1.5}
-                                    width={'100%'}
-                                    sx={{
-                                        backgroundColor: 'black',
-                                        transition: '0.2s',
-                                        '&:hover': {
-                                            backgroundColor: 'lightgray'
-                                        }
-                                    }}
-                                >
-                                    <NavigationText data-text={item.name}>
-                                        {item.name}
-                                    </NavigationText>
-                                </PadBox>
-                            )
-                        })}
-                    </FlexBox>
-                </FlexBox>
+                <DropDownMenu
+                    render={() => (
+                        <>
+                            {items.map((item) => {
+                                return (
+                                    <PadBox
+                                        key={item.name}
+                                        padding={1.5}
+                                        width={'100%'}
+                                        sx={{
+                                            backgroundColor: 'black',
+                                            transition: '0.2s',
+                                            '&:hover': {
+                                                backgroundColor: 'lightgray'
+                                            }
+                                        }}
+                                    >
+                                        <NavigationText data-text={item.name}>
+                                            {item.name}
+                                        </NavigationText>
+                                    </PadBox>
+                                )
+                            })}
+                        </>
+                    )}
+                />
             </FlexBox>
         </FlexBox >
     )

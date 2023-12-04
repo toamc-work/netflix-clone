7fa23ed3288f71e9a7f7c9eec812b98419fd4f8f
toamc-work <toamc.work@gmail.com>
2023-12-04 03:01:13 +0200

    Enhance NavigationSideMenu component - Refactor handling of hover events for better code organization

--- src/components/navbar/components/navigationSideMenu/navigationSideMenu.tsx
+++ src/components/navbar/components/navigationSideMenu/navigationSideMenu.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { FlexBox, NavigationText, PadBox } from '../../../mui/MuiStyledComponents';
 import { ArrowDropDown, Notifications, Search } from '@mui/icons-material';
 import { useMediaQuery } from 'usehooks-ts';
@@ -11,6 +11,39 @@ const NavigationSideMenu: React.FC<INavigationSideMenuProps> = ({ isDesktop }) =
     const searchIconMaxView = useMediaQuery('(max-width: 1000px)')
     const kidsIconMaxView = useMediaQuery('(max-width: 900px)')
     const NotificationIconMaxView = useMediaQuery('(max-width: 800px)')
+
+    const [isHoverOnMenuIcon, setIsHoverOnMenuIcon] = useState(false);
+    const [isHoverOnMenu, setIsHoverOnMenu] = useState(false);
+
+
+    const handleIsHoverOnMenuIcon = (mouse: 'enter' | 'leave') => {
+        if (mouse === 'enter') {
+            setIsHoverOnMenuIcon(true)
+        }
+
+        if (mouse === 'leave') {
+
+            const timeoutID = setTimeout(() => {
+                setIsHoverOnMenuIcon(() => false);
+                clearTimeout(timeoutID)
+            }, 200);
+        }
+
+    }
+
+    const handleIsHoverOnMenu = (mouse: 'enter' | 'leave') => {
+        if (mouse === 'enter') {
+            setIsHoverOnMenu(true)
+        }
+
+        if (mouse === 'leave') {
+            const timeoutID = setTimeout(() => {
+                setIsHoverOnMenu(() => false);
+                clearTimeout(timeoutID)
+            }, 200);
+        }
+
+    }
 
     return (
         <FlexBox
